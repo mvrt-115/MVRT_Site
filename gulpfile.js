1310825ad3ac3dd53070f49fcca54650346d68f4
David Liu <a.davidliu@gmail.com>
2015-02-16 21:21:11 -0800

    refactor and document gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
+// gulpfile.js
+// Most of the build tasks go in here.
+// See https://github.com/gulpjs/gulp/tree/master/docs
+
+// Dependencies/Plugins
+// Gulp plugins are prefixed with a `$`
 var gulp = require('gulp')
   , $ = require('gulp-load-plugins')({
-      rename: {
-        'gulp-util': 'gutil',
-        'gulp-autoprefixer': 'prefix',
-        'gulp-ruby-sass': 'sass',
-      }
+      rename: { 'gulp-ruby-sass': 'sass' }
     })
   , del = require('del')
   , browserify = require('browserify')
@@ -13,139 +15,114 @@ var gulp = require('gulp')
   , buffer = require('vinyl-buffer')
   , browserSync = require('browser-sync')
   , reload = browserSync.reload
-  , runSequence = require('run-sequence')
-  , exec = require('child_process').exec
+  , spawn = require('win-spawn')
+  , dargs = require('dargs')
+  , merge = require('merge-stream')
 
-var merge = require('merge-stream')
+// A "flag", set to true if in production mode.
+var production = false
 
-var paths = {
-  css: './app/_scss', // no globbing
-  images: ['./app/img/**/*'], // doesnt really matter
-  svg: ['./app/img/svg/**/*'], // once there will be some
-  fonts: ['./app/fonts'], // will come later
-  js: ['./app/js/index.js'] // browserify
-}
+// bs-reload for reloading HTML
+gulp.task('bs-reload', function () { reload() })
 
-var production = false // set true if in production mode
+// Serves files for development.
+gulp.task('serve', ['jekyll', 'stylesheets', 'javascripts'], function () {
 
-// development serve
-gulp.task('serve', ['jekyll', 'css', 'js'], function () {
   browserSync.init({
-    server: {
-      baseDir: ['.jekyll', '.tmp', 'app'],
-    },
+    server: { baseDir: ['.tmp', 'app'] },
     port: 4000
   })
 
   // watch and recompiles files
   gulp.watch([
-    '_config.yml',
-    'app/**/*.{html,yml,md,mkd,markdown,json}',
-    '!app/_bower_components/**/*'
-  ], ['jekyll', reload])
-  gulp.watch([paths.css + '/**/*.scss'], ['css'])
-  gulp.watch(['app/js/**/*.js'], ['jshint'])
-  // browserify watches the javascripts
+    './_config.yml',
+    './app/**/*.{html,yml,md,mkd,markdown,json}',
+    '!./app/_bower_components/**/*'
+  ], ['jekyll'])
+  gulp.watch(['./.tmp/**/*.html'], ['bs-reload'])
+  gulp.watch(['./app/_scss/**/*.scss'], ['stylesheets'])
+  gulp.watch(['./app/js/**/*.js'], ['jshint'])
 
 })
 
-
-// serve production stuff
-// DONT USE ON SERVER...I think
-gulp.task('serve:dist', ['production'], function () {
+// Serves the production mode.
+gulp.task('serve:dist', ['build'], function () {
   browserSync.init({
-    server: {
-      baseDir: 'dist'
-    },
+    server: { baseDir: 'dist' },
     port: 4000,
     notify: false
   })
 })
 
-// scss -> autoprefixer -> good ol' css
-gulp.task('css', function () {
-  var options = { bundleExec: true, require: ['bourbon', 'neat'] }
-  options.sourcemap = production ? false : true
-  return $.sass(paths.css, options)
-    .on('error', $.gutil.log.bind($.gutil))
-    .pipe($.prefix())
-    .pipe($.if(!production, $.sourcemaps.write()))
-    .pipe(gulp.dest('.tmp/css'))
-    .pipe($.if(!production, reload({ stream: true })))
-    .pipe($.size({ title: 'styles preminification' }))
-})
+// Mini Tasks
+gulp.task('stylesheets', stylesheets)
+gulp.task('javascripts', javascripts)
+gulp.task('jekyll', jekyll)
+gulp.task('images', images)
 
-// javascripts with browserify
-gulp.task('js', function () {
-  var bOptions = { entries: [paths.js], debug: true }
-  var b = production ? browserify(bOptions) :
-    watchify(browserify(bOptions), watchify.args)
-  if (!production) b.on('update', bundle)
+// Stylesheets stream/task.
+// Sass => Autoprefixer => Unminified CSS File
+function stylesheets () {
+  var opts = {
+    bundleExec: true,
+    require: ['bourbon', 'neat'],
+    sourcemap: production ? false : true
+  }
+  return $.sass('./app/_scss/main.scss', opts)
+    .on('error', function (err) { $.util.log(err.message) })
+    .pipe($.autoprefixer())
+    .pipe($.if(!production, $.sourcemaps.write()))
+    .pipe($.rename(function (path) { path.dirname = 'css' }))
+    .pipe(gulp.dest('./.tmp'))
+    .pipe($.if(!production, reload({ stream: true })))
+    .pipe($.size({ title: 'stylesheets preminification' }))
+}
+
+// Javascripts stream/task.
+// Utilizes Browserify + Crew
+function javascripts () {
+  var opts = { entries: ['./app/js/index.js'], debug: true }
+  var b = production
+    ? browserify(opts)
+    : watchify(browserify(opts), watchify.args).on('update', bundle)
   function bundle () {
-    return b.bundle()
-      .on('error', $.gutil.log)
+    return b
+      .transform('browserify-shim')
+      .bundle()
+      .on('error', function (err) { $.util.log('Browserify Error:', err.message) })
       .pipe(source('script.js'))
       .pipe(buffer())
       .pipe($.sourcemaps.init({ loadMaps: true }))
       .pipe($.if(!production, $.sourcemaps.write()))
-      .pipe(gulp.dest('.tmp/js'))
+      .pipe($.rename(function (path) { path.dirname = 'js' }))
+      .pipe(gulp.dest('./.tmp'))
       .pipe($.if(!production, reload({ stream: true })))
       .pipe($.size({ title: 'javascripts preminification' }))
   }
   return bundle()
-})
+}
 
-// jekyll
-gulp.task('jekyll', function (done) {
-  if (production) {
-    exec('bundle exec jekyll build --source app --config _config.yml,_config.build.yml --destination dist', done)
-  } else {
-    exec('bundle exec jekyll build --source app --config _config.yml --destination .jekyll', done)
-  }
-})
+// Jekyll stream/task.
+// Jekyll is funny in the sense that streaming is not possible.
+// So we just write the files to a special .jekyll directory.
+// And source that later and pipe that through a noop stream.
+function jekyll () {
+  var tr = $.util.noop()
+  var cmd = ['bundle', 'exec', 'jekyll', 'build'].concat(dargs({
+    source: 'app',
+    config: production ? '_config.yml,_config.build.yml' : '_config.yml',
+    destination: '.jekyll'
+  }))
+  spawn(cmd.shift(), cmd, { stdio: 'inherit' })
+    .on('close', function () { gulp.src('./.jekyll/**/{.*,*}').pipe(tr) })
+  return tr
+    .pipe(gulp.dest('./.tmp'))
+}
 
-// Compiles HTML
-gulp.task('html', function () {
-
-  var jsFilter = $.filter('**/*.js')
-    , cssFilter = $.filter('**/*.css')
-    , htmlFilter = $.filter('**/*.html')
-    , indexFilter = $.filter('index.html')
-
-  var assets = $.useref.assets({ searchPath: '{.tmp,app}' })
-
-  return gulp.src('dist/**/*.html')
-    .pipe(indexFilter) // only source from index.html
-    .pipe(assets)
-    .pipe(jsFilter)
-    .pipe($.uglify())
-    .pipe($.size({ title: 'scripts postminification' }))
-    .pipe(jsFilter.restore())
-    .pipe(cssFilter)
-    .pipe($.minifyCss())
-    .pipe($.size({ title: 'styles postminification' }))
-    .pipe(cssFilter.restore())
-    .pipe($.rev())
-    .pipe(assets.restore())
-    .pipe(indexFilter.restore()) // now useref on on all the files
-    .pipe($.useref())
-    .pipe($.revReplace())
-    .pipe(htmlFilter)
-    .pipe($.size({ title: 'html preminification' }))
-    .pipe($.minifyHtml())
-    .pipe($.size({ title: 'html postminification' }))
-    .pipe(htmlFilter.restore())
-    .pipe(gulp.dest('dist'))
-
-})
-
-// Revs images, replace links in other files
-gulp.task('img', function () {
-
-  var imgFilter = $.filter('**/*.{jpg,png,gif,svg,webp}')
-
-  return gulp.src('dist/**/*')
-    .pipe(imgFilter)
+// Images stream/task.
+// In short, compresses images.
+function images () {
+  return gulp.src('./app/img/**/*')
     .pipe($.size({ title: 'images preminification' }))
     .pipe($.imagemin({
       progressive: true,
@@ -153,43 +130,80 @@ gulp.task('img', function () {
       use: [require('imagemin-pngquant')()]
     }))
     .pipe($.size({ title: 'images postminification' }))
+    .pipe($.rename(function (path) { path.dirname = 'img/' + path.dirname }))
+    .pipe(gulp.dest('./.tmp'))
+}
+
+// Assembles the site task.
+// Actually, there is an assemble package on npm that is like Jekyll but for
+// Node/io.js
+gulp.task('assemble', ['clean', 'jshint'], function () {
+
+  // Sets production to true.
+  production = true
+
+  // Various filters to filter out certain files.
+  var imgFilter = $.filter('**/*.{jpg,png,gif,svg,webp}')
+    , jsFilter = $.filter('**/*.js')
+    , cssFilter = $.filter('**/*.css')
+    , htmlFilter = $.filter('**/*.html')
+    , index = $.filter('index.html')
+    , assets = $.useref.assets({ searchPath: '{.tmp,app}' })
+
+  // This is a gigantic stream that in short
+  // a) Minifies stuff, and
+  // b) revisions stuff
+  return merge()
+    .add(stylesheets())
+    .add(javascripts())
+    .add(images())
+    .add(jekyll())
+    .pipe(gulp.dest('./dist'))
+    .pipe(imgFilter)
     .pipe($.rev())
     .pipe(imgFilter.restore())
     .pipe($.revReplace())
-    .pipe(gulp.dest('dist'))
+    .pipe(index)
+    .pipe(assets)
+    .pipe(cssFilter)
+    .pipe($.minifyCss())
+    .pipe($.size({ title: 'stylesheets postminification' }))
+    .pipe(cssFilter.restore())
+    .pipe(jsFilter)
+    .pipe($.uglify())
+    .pipe($.size({ title: 'javascripts postminification' }))
+    .pipe(jsFilter.restore())
+    .pipe($.rev())
+    .pipe(assets.restore())
+    .pipe(index.restore())
+    .pipe($.revReplace())
+    .pipe(htmlFilter)
+    .pipe($.useref())
+    .pipe($.size({ title: 'html preminification' }))
+    .pipe($.minifyHtml())
+    .pipe($.size({ title: 'html postminification' }))
+    .pipe(htmlFilter.restore())
+    .pipe(gulp.dest('./dist'))
+
 })
 
-// copies stuff
-gulp.task('copy', function () {
-  var images = gulp.src('app/img/**/*', { base: 'app' })
-    .pipe(gulp.dest('dist'))
-  var htaccess = gulp.src('app/**/*.htaccess', { base: 'app'})
-    .pipe(gulp.dest('dist'))
-  return merge(htaccess, images)
-})
-
-gulp.task('production', ['clean'], function (cb) {
-  production = true
-  runSequence(
-    'jshint',
-    ['css', 'js', 'jekyll', 'copy'],
-    'html',
-    'img',
-    cb
-  )
-})
-
-// clean
+// Clean up messes.
 gulp.task('clean', function (done) {
   del(['.tmp', '.jekyll', 'dist'], done)
 })
 
+// JSHint - for code quality and error checking.
+// Note that currently if JSHint fails on build then the site won't compile.
 gulp.task('jshint', function () {
-  return gulp.src('app/js/**/*.js')
+  return gulp.src('./app/js/**/*.js')
     .pipe($.jshint())
     .pipe($.jshint.reporter(require('jshint-stylish')))
     .pipe($.if(!browserSync.active, $.jshint.reporter('fail'))) // http://bit.ly/14lPNoj
 })
 
+// Aliases for assemble.
+gulp.task('build', ['assemble'])
+gulp.task('production', ['assemble'])
 
-gulp.task('default', ['production'])
+// Default task an alias for assemble.
+gulp.task('default', ['assemble'])
