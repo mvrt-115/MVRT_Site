75904358a3dd5d2bdb2f183ba931f045ff480264
akalari <aditya.kalari@gmail.com>
2015-09-23 17:48:02 -0700

    Finally fixed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -19,9 +19,10 @@ var gulp = require('gulp')
 var merge = require('merge-stream')
 
 var paths = {
-  css: ['./app/assets/css'], // no globbing
-  images: ['./app/assets/img/**/*'], // doesnt really matter
+  css: './app/assets/css', // no globbing
+  images: ['./app/assets/images/**/*'], // doesnt really matter
   svg: ['./app/img/svg/**/*'], // once there will be some
+  fonts: ['./app/fonts'], // will come later
   js: ['./app/assets/js/main.js'] // browserify
 }
 
