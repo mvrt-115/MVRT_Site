af2a936e4ac14158c662e965243e06291139f9eb
akalari <aditya.kalari@gmail.com>
2015-10-11 13:43:06 -0700

    INIT SCRIPTS

--- mac-init.sh
+++ mac-init.sh
@@ -1,7 +1,24 @@
+# Running commands as sudo means you need the administrator login
+
+echo "Make sure that you are root on your computer (Administrator)"
+
+# Bundler
+echo "Installing bundler"
 sudo gem install bundler
-bundle install
+bundle installi
+
+# Jekyll
+echo "Installing Jekyll"
 sudo gem install jekyll
+
+# NODE and GULP
+echo "Installing Bower"
 sudo npm install -g bower
+bower install
+echo "Installing gulp"
 sudo npm install -g gulp
 npm install
+
+# HACKING
+echo "FINISHED INSTALLATION: ENTERING TESTING"
 gulp serve
