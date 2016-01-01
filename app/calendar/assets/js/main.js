e034dfa6babff583f1fa0895db884fc30200ebd1
akalari <aditya.kalari@gmail.com>
2015-12-31 19:02:04 -0800

    New Changes

--- app/calendar/assets/js/main.js
+++ app/calendar/assets/js/main.js
@@ -39,9 +39,9 @@
 
 					// Images (in the format of 'url': 'alignment').
 						images: {
-							'images/bg01.jpg': 'center',
-							'images/bg02.jpg': 'center',
-							'images/bg03.jpg': 'center'
+							'../images/countdown//bg01.jpg': 'center',
+							'../images/countdown/bg02.jpg': 'center',
+							'../images/countdown/bg03.jpg': 'center'
 						},
 
 					// Delay.
