ad5b5049d15c68461e5efc7d3d4a516fe30e5bdf
akalari <aditya.kalari@gmail.com>
2016-07-17 12:02:22 -0700

    lmao

--- Gemfile
+++
@@ -1,8 +0,0 @@
-source "https://rubygems.org"
-
-gem 'jekyll'
-gem 'kramdown'
-gem 'rouge'
-gem 'sass'
-gem 'bourbon'
-gem 'neat'
--- app/assets/js/forcedraw.js
+++
@@ -1,6 +0,0 @@
-function forceredraw() {
-  var banner = document.getElementById('banner');
-  banner.style.display='none';
-  banner.offsetHeight;
-  banner.style.display='block';
-}
